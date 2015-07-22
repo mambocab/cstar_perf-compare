900ea4e53570aa836e7278109bfa00665f7105db
Jim Witschey <jim.witschey@gmail.com>
2015-07-22 21:19:22 +0000

    more output and tests

--- t.sh
+++ t.sh
@@ -1,8 +1,13 @@
 for v in git:trunk git:cassandra-2.2 ; do
-  set -x
-  ccm remove
-  ccm create trunk-long-column-names -n 3 -v $v
-  ccm start --wait-for-binary-proto
-  ccm stress -- user 'profile=long_names.yaml' n=500K ops\(insert=1\) -rate threads=10
-  python analyze.py
+  for c in 100 500 1000 ; do
+    set -x
+    set +e
+    ccm remove
+    set -e
+    ccm create trunk-long-column-names -n 3 -v $v
+    ccm start --wait-for-binary-proto
+    # ccm stress -- user 'profile=long_names.yaml' n=500K ops\(insert=1\) -rate threads=10
+    ccm stress -- write n=500K -rate threads=5 -col n=FIXED\($c\)
+    python analyze.py
+  done
 done
